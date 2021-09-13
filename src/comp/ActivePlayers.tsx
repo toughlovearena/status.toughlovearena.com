16d4394eb246aa7b586c2d4da93c94d4cb162437
M. Paul Weeks <mpaulweeks@gmail.com>
2021-09-13 13:12:37 -0400

    new presence types

--- src/comp/ActivePlayers.tsx
+++ src/comp/ActivePlayers.tsx
@@ -34,32 +34,62 @@ export function ActivePlayers() {
           <tr>
             <td>Welcome</td>
             <td>{renderCount('welcome')}</td>
+            <td>Menu</td>
+            <td>{renderCount('menu')}</td>
+          </tr>
+          <tr>
+            <td>How to Play</td>
+            <td>{renderCount('how_to_play')}</td>
             <td>Ranked Match</td>
             <td>{renderCount('online_ranked')}</td>
           </tr>
           <tr>
-            <td>Tutorial</td>
-            <td>{renderCount('tutorial')}</td>
+            <td>Local Versus</td>
+            <td>{renderCount('vs_local')}</td>
             <td>Casual Match</td>
             <td>{renderCount('online_casual')}</td>
           </tr>
           <tr>
-            <td>Local Versus</td>
-            <td>{renderCount('local')}</td>
+            <td>Versus CPU</td>
+            <td>{renderCount('vs_cpu')}</td>
             <td>Private Match</td>
             <td>{renderCount('online_private')}</td>
           </tr>
           <tr>
-            <td>Versus CPU</td>
-            <td>{renderCount('cpu')}</td>
+            <td>Boss Fight</td>
+            <td>{renderCount('vs_boss')}</td>
             <td>Private Lobby</td>
             <td>{renderCount('online_lobby')}</td>
           </tr>
           <tr>
             <td>Training</td>
             <td>{renderCount('training')}</td>
+            <td>Account Login</td>
+            <td>{renderCount('online_account')}</td>
+          </tr>
+          <tr>
+            <td>Frame Data</td>
+            <td>{renderCount('frame_data')}</td>
+            <td>Account Stats</td>
+            <td>{renderCount('online_stats')}</td>
+          </tr>
+          <tr>
+            <td>Debug Ref</td>
+            <td>{renderCount('debug_ref')}</td>
+            <td>Leaderboard</td>
+            <td>{renderCount('online_leaderboard')}</td>
+          </tr>
+          <tr>
+            <td>About</td>
+            <td>{renderCount('about')}</td>
+            <td>Hall of Fame</td>
+            <td>{renderCount('hall_of_fame')}</td>
+          </tr>
+          <tr>
             <td>Modding</td>
             <td>{renderCount('modding')}</td>
+            <td>Replay</td>
+            <td>{renderCount('replay')}</td>
           </tr>
         </tbody>
       </DataTable>
