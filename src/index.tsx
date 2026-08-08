84a8322d95fbe11e10d8a12b7bc748f8db110a73
mpaulweeks <mpaulweeks@gmail.com>
2026-08-08 14:21:00 -0400

    update react-scripts, fix js error

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './comp';
-import reportWebVitals from './reportWebVitals';
-import { isLongPoll } from './util';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./comp";
+import reportWebVitals from "./reportWebVitals";
+import { isLongPoll } from "./util";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
 // hard refresh every 5 mins
 const refreshPeriod = (isLongPoll() ? 60 : 5) * 60 * 1000;
-setInterval(() => window.location.reload(false), refreshPeriod);
+setInterval(() => window.location.reload(), refreshPeriod);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
